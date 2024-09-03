5bfb0072742e5f6370d841a25f5d2c91c0b18728
Eduardo Mambrilla Martínez <emambrilla@gmail.com>
2024-09-03 13:30:02 +0200

    Segunda UT, ej2

--- app/src/main/java/com/emambrilla/androidtraining/MainActivity.kt
+++ app/src/main/java/com/emambrilla/androidtraining/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.emambrilla.androidtraining
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +10,34 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        Log.d("MainActivity", "OnCreate")
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d( "MainActivity", "OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d( "MainActivity", "OnResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d( "MainActivity", "OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d( "MainActivity", "OnStop")
     }
 }
