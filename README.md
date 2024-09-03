d5147045c9b0e504a1e6841dc8c547a54a1620e5
Eduardo Mambrilla Martínez <emambrilla@gmail.com>
2024-09-03 14:08:53 +0200

    Segunda UT, ej2

--- README.md
+++ README.md
@@ -21,3 +21,4 @@ Repositorio con los ejercicios y contenidos vistos en el curso de Formación del
 
 **Ejercicios**
 - [Ejercicio 1](docs/ut02/ut2-ej1.pdf)
+- [Ejercicio 2](docs/ut02/ut2-ej2.pdf)
--- app/src/main/java/com/emambrilla/androidtraining/MainActivity.kt
+++ app/src/main/java/com/emambrilla/androidtraining/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         Log.d("MainActivity", "OnCreate")
 //        enableEdgeToEdge()
 //        setContentView(R.layout.activity_main)
